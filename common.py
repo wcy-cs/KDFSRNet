9bacf73836d499dc03cc3319229eba490287d8b2
wcy-cs <39185517+wcy-cs@users.noreply.github.com>
2022-09-29 15:37:23 +0800

    Update common.py

--- common.py
+++ common.py
@@ -8,6 +8,16 @@ def default_conv(in_channels, out_channels, kernel_size, stride=1, bias=True):
         stride=stride, padding=(kernel_size // 2), bias=bias)
 
 
+class MeanShift(nn.Conv2d):
+    def __init__(self, rgb_range, rgb_mean=(0.4488, 0.4371, 0.4040), rgb_std=(1.0, 1.0, 1.0), sign=-1):
+
+        super(MeanShift, self).__init__(3, 3, kernel_size=1)
+        std = torch.Tensor(rgb_std)
+        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
+        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean) / std
+        for p in self.parameters():
+            p.requires_grad = False
+            
 class Upsampler(nn.Sequential):
     def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
 
