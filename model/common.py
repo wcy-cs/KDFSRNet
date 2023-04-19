82fdf05c790dc07d21701f51c0ba42919d1b0544
wcy-cs <39185517+wcy-cs@users.noreply.github.com>
2023-04-19 19:51:35 +0800

    Create common.py

---
+++ model/common.py
@@ -0,0 +1,114 @@
+import torch.nn as nn
+import torch
+import math
+
+def default_conv(in_channels, out_channels, kernel_size, stride=1, bias=True):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        stride=stride, padding=(kernel_size // 2), bias=bias)
+
+
+class MeanShift(nn.Conv2d):
+    def __init__(self, rgb_range, rgb_mean=(0.4488, 0.4371, 0.4040), rgb_std=(1.0, 1.0, 1.0), sign=-1):
+
+        super(MeanShift, self).__init__(3, 3, kernel_size=1)
+        std = torch.Tensor(rgb_std)
+        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
+        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean) / std
+        for p in self.parameters():
+            p.requires_grad = False
+            
+class Upsampler(nn.Sequential):
+    def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
+
+        m = []
+        if (scale & (scale - 1)) == 0:  # Is scale = 2^n?
+            for _ in range(int(math.log(scale, 2))):
+                m.append(conv(n_feats, 4 * n_feats, 3, bias))
+                m.append(nn.PixelShuffle(2))
+                if bn:
+                    m.append(nn.BatchNorm2d(n_feats))
+                if act == 'relu':
+                    m.append(nn.ReLU(True))
+                elif act == 'prelu':
+                    m.append(nn.PReLU(n_feats))
+
+        elif scale == 3:
+            m.append(conv(n_feats, 9 * n_feats, 3, bias))
+            m.append(nn.PixelShuffle(3))
+            if bn:
+                m.append(nn.BatchNorm2d(n_feats))
+            if act == 'relu':
+                m.append(nn.ReLU(True))
+            elif act == 'prelu':
+                m.append(nn.PReLU(n_feats))
+        else:
+            raise NotImplementedError
+
+        super(Upsampler, self).__init__(*m)
+
+class Upsampler_module(nn.Module):
+    def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
+        super(Upsampler_module, self).__init__()
+       
+        self.up = nn.PixelShuffle(2)
+        self.conv = nn.Conv2d(in_channels=n_feats, out_channels=n_feats*4, kernel_size=3, stride=1, padding=1)
+    def forward(self, x):
+        # print(x.shape)
+        x = self.conv(x)
+        # print(x.shape)
+        x = self.up(x)
+        return x
+class invPixelShuffle(nn.Module):
+
+    def __init__(self, ratio=2):
+        super(invPixelShuffle, self).__init__()
+        self.ratio = ratio
+
+    def forward(self, tensor):
+        ratio = self.ratio
+        b = tensor.size(0)
+        ch = tensor.size(1)
+        y = tensor.size(2)
+        x = tensor.size(3)
+        assert x % ratio == 0 and y % ratio == 0, 'x, y, ratio : {}, {}, {}'.format(x, y, ratio)
+
+        return tensor.view(b, ch, y // ratio, ratio, x // ratio, ratio).permute(0, 1, 3, 5, 2, 4).contiguous().view(b,
+                                                                                                                    -1,
+                                                                                                                    y // ratio,
+                                                                                                                   x // ratio)
+
+class invUpsampler(nn.Sequential):
+    def __init__(self, conv, scale, n_feat, bn=False, act=False, bias=True):
+
+        m = []
+        if (scale & (scale - 1)) == 0:  # Is scale = 2^n?
+            for _ in range(int(math.log(scale, 2))):
+                m.append(invPixelShuffle(2))
+                m.append(conv(n_feat * 4, n_feat, 3, bias))
+                if bn: m.append(nn.BatchNorm2d(n_feat))
+                if act: m.append(act())
+        elif scale == 3:
+            m.append(invPixelShuffle(3))
+            m.append(conv(n_feat * 9, n_feat, 3, bias))
+            if bn: m.append(nn.BatchNorm2d(n_feat))
+            if act: m.append(act())
+        else:
+            raise NotImplementedError
+
+        super(invUpsampler, self).__init__(*m)
+        
+class invUpsampler_module(nn.Module):
+    def __init__(self, conv, scale, n_feat, bn=False, act=False, bias=True):
+        super(invUpsampler_module, self).__init__()
+
+        self.up = invPixelShuffle(2)
+        self.conv = nn.Conv2d(in_channels=n_feat*4, out_channels=n_feat, kernel_size=3,
+        stride=1, padding=1) #conv(n_feat*4, n_feat, 3, bias)
+
+    def forward(self, x):
+        x = self.up(x)
+        # print(x.shape)
+        x = self.conv(x)
+        return x
+
